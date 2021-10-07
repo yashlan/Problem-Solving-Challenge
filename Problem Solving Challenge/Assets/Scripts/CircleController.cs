96bda43766c6cab8cd13a11b6a6f3e40cccc882a
yashlan <yashlan007@gmail.com>
2021-10-07 14:51:45 +0700

    update

--- Problem Solving Challenge/Assets/Scripts/CircleController.cs
+++ Problem Solving Challenge/Assets/Scripts/CircleController.cs
@@ -1,11 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Yashlan.controller 
 {
+    public enum ProblemType
+    {
+        problem_2,
+        problem_3,
+        problem_4,
+        problem_5,
+        problem_6,
+        problem_7,
+        problem_8,
+        problem_9,
+        problem_10,
+    }
     public class CircleController : MonoBehaviour
     {
+        [SerializeField]
+        private ProblemType _problemType;
         [SerializeField]
         private Rigidbody2D _rb;
         [SerializeField]
@@ -13,13 +25,47 @@ namespace Yashlan.controller
 
         Vector2 movement;
 
-        void Update()
+
+        void Start() 
         {
-            movement.x = Input.GetAxisRaw("Horizontal");
-            movement.y = Input.GetAxisRaw("Vertical");
+            if(_problemType == ProblemType.problem_2)
+            {
+                _speed = 2f;
+                _rb.velocity = new Vector2(_speed, _rb.velocity.y);
+            }
+
+            if (_problemType == ProblemType.problem_3)
+            {
+                _speed = 1000f;
+                _rb.AddForce(new Vector2(_speed, _speed));
+            }
+        } 
+
+        void OnGUI()
+        {
+            if (_problemType == ProblemType.problem_2 || _problemType == ProblemType.problem_3)
+            {
+                var speed_info = $"Move Speed : {_rb.velocity.magnitude}f";
+                var guiStyle = new GUIStyle(GUI.skin.textArea);
+                guiStyle.alignment = TextAnchor.MiddleCenter;
+                guiStyle.fontSize = 20;
+                GUI.TextArea(new Rect(Screen.width / 2 - 200, Screen.height - 200, 400, 110), speed_info, guiStyle);
+            }
         }
 
-        void FixedUpdate() => 
-            _rb.MovePosition(_rb.position + movement * _speed * Time.fixedDeltaTime);
+        void Update()
+        {
+            if(_problemType == ProblemType.problem_4)
+            {
+                movement.x = Input.GetAxisRaw("Horizontal");
+                movement.y = Input.GetAxisRaw("Vertical");
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (_problemType == ProblemType.problem_4)
+                _rb.MovePosition(_rb.position + movement * _speed * Time.fixedDeltaTime);
+        }
     }
 }
