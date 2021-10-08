823f04af76152dab95036f4131636a8e6e6e5d42
yashlan <yashlan007@gmail.com>
2021-10-08 15:12:53 +0700

    update scripts

--- Problem Solving Challenge/Assets/Scripts/CircleController.cs
+++ Problem Solving Challenge/Assets/Scripts/CircleController.cs
@@ -1,23 +1,12 @@
 ﻿using UnityEngine;
+using Yashlan.enums;
 
 namespace Yashlan.controller 
 {
-    public enum ProblemType
-    {
-        problem_2,
-        problem_3,
-        problem_4,
-        problem_5,
-        problem_6,
-        problem_7,
-        problem_8,
-        problem_9,
-        problem_10,
-    }
     public class CircleController : MonoBehaviour
     {
         [SerializeField]
-        private ProblemType _problemType;
+        private ProblemTypes.ProblemType _problemType;
         [SerializeField]
         private Rigidbody2D _rb;
         [SerializeField]
@@ -25,7 +14,7 @@ namespace Yashlan.controller
 
         void Start()
         {
-            if (_problemType == ProblemType.problem_2)
+            if (_problemType == ProblemTypes.ProblemType.problem_2)
             {
                 _speed = 2f;
                 _rb.velocity = new Vector2(_speed, _rb.velocity.y);
@@ -34,7 +23,7 @@ namespace Yashlan.controller
 
         void OnGUI()
         {
-            if (_problemType == ProblemType.problem_2)
+            if (_problemType == ProblemTypes.ProblemType.problem_2)
             {
                 var speed_info = $"Move Speed : {_rb.velocity.magnitude}f";
                 var guiStyle = new GUIStyle(GUI.skin.textArea);
