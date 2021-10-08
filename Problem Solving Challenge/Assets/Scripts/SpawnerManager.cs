920e5ad5068ab07b342c9344d683e5675b0a1068
yashlan <yashlan007@gmail.com>
2021-10-08 14:45:17 +0700

    problem 6 done

---
+++ Problem Solving Challenge/Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Yashlan.manage
+{
+    #region untuk problem 6
+    public class SpawnerManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _box;
+        [SerializeField]
+        private Sprite[] _boxSprites;
+
+        void Start()
+        {
+            Spawn();
+        }
+
+        void Spawn()
+        {
+            var spawnLength = Random.Range(5, 15);
+            for (int i = 0; i < spawnLength; i++)
+            {
+                var sr = _box.GetComponent<SpriteRenderer>();
+                sr.sprite = _boxSprites[Random.Range(0, _boxSprites.Length)];
+                Instantiate(_box, new Vector3(Random.Range(-8f, 8f), Random.Range(-4f, 4f), _box.transform.position.z), Quaternion.identity);
+            }
+        }
+    }
+    #endregion
+}
